ac42c9ae3d15e6e9ef6b552d7c90d58b2077d758
Fred Jacobs <thefredjacobs@gmail.com>
2021-05-25 12:27:11 +0200

    2021-05-25

--- src/components/TestingStyling.vue
+++ src/components/TestingStyling.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    <v-container fluid fill-height>
-      <row no-gutters :class="{ 'd-block': isSmallerScreens() }">
-        <v-col> Whaaaaaat</v-col>
+    <v-container fluid fill-height pa-0>
+      <v-row
+        no-gutters
+        :class="{ 'd-block': isSmallerScreens() }"
+        height="100%"
+      >
+        <v-col 
+          class="blue darken-4 home-logo-container d-flex"
+          align-self="center"
+        >
+          <v-img src="@/assets/KINGDOM.png" width="300" height="300"></v-img>
+        </v-col>
 
-        <v-col> </v-col>
-      </row>
+        <v-col 
+          class="white lighten-3 pa-10 d-flex"
+          :class="isSmallerScreens() ? 'full-form' : 'form-minimum'"
+        >
+          <v-form ref="form" lazy-validation>
+            <v-text-field label="Name" required></v-text-field>
+
+            <v-text-field label="E-mail" required></v-text-field>
+
+            <v-select label="Item" required></v-select>
+
+            <v-checkbox label="Do you agree?" required></v-checkbox>
+
+            <v-btn color="success" class="mr-4"> SEND </v-btn>
+
+           
+          </v-form>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -20,4 +46,14 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.form-minimum {
+  max-width: 640px;
+}
+.full-form {
+  width: 100%;
+}
+.home-logo-container {
+  max-width: calc(100% - 640px);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
 import AboutUs from "@/components/AboutUs.vue";
 import ClientSelect from "@/components/ClientSelect.vue";
 import TestRoutesPage from "@/components/TestRoutesPage.vue";
-import TestingStyling from "@/components/TestingStyling.vue";
+
 
 export default {
   data: function () {
@@ -20,7 +20,7 @@ export default {
         aboutus: "AboutUs",
         clientselect: "ClientSelect",
         test: "TestRoutesPage",
-        teststyling:"TestingStyling",
+        
       },
     };
   },
@@ -28,7 +28,7 @@ export default {
     AboutUs: AboutUs,
     ClientSelect: ClientSelect,
     TestRoutesPage: TestRoutesPage,
-    TestingStyling:TestingStyling,
+    
   },
 
   mounted() {
