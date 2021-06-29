50e2627b0f5244d52b64ace0b0f03eb4d8424948
ian pretorius <ian.pretorius.sa@gmail.com>
2021-06-29 14:37:01 +0200

    Merge branch 'master' of https://github.com/fredjacobs/fred_test_vue

--- src/components/CaptureDataTextField.vue
+++ src/components/CaptureDataTextField.vue
@@ -1,40 +1,37 @@
 <template>
-  <div >
-  
-    <v-card class="pa-6 " elevation="2">
-      <v-text-field
+  <div class="">
+    <v-card class="pa-6"  elevation="2">
+      <component
+        :is="type"
         :label="label"
-        v-model="input"
+        v-model="data_input"
         clearable
         :prepend-icon="icon"
         :append-outer-icon="click_icon"
-        @click:append-outer="add_function"
-      ></v-text-field>
-
-      
+        @click:append-outer="add_item"
+        v-bind="extra_attributes"
+      ></component>
 
       <v-list-item
         class="grey lighten-3 pa-2 mb-2"
         align="center"
         justify="space-around"
-        v-for="cur_entry in form_data"
+        v-for="cur_entry in data_items"
         :key="cur_entry.key"
       >
         <v-list-item-content>
-          <v-list-item-title>{{ cur_entry.entry }}</v-list-item-title>
+          <v-list-item-title>{{ cur_entry.value }}</v-list-item-title>
           <div class="d-flex justify-center">
             <v-checkbox
               :input-value="active"
               label="Primary"
               append-icon="mdi-trash-can-outline"
-              @click:append="delete_entry"
+              @click:append="delete_item($event,cur_entry)"
             ></v-checkbox>
           </div>
         </v-list-item-content>
       </v-list-item>
-      <v-list-item 
-        >no entries yet</v-list-item
-      >
+      <v-list-item>no entries yet</v-list-item>
     </v-card>
   </div>
 </template>
@@ -42,16 +39,60 @@
 export default {
   data: function () {
     return {
-      input: null,
+      data_input: null,
+      data_items: {},
+      data_key: 1,
     };
   },
-  props: ["label", "value", "icon", "click_icon", "form_data", "active"],
+  props: [
+    "label",
+    "value",
+    "icon",
+    "click_icon",    
+    "active",
+    "type",
+    "extra_attributes",
+  ],
   methods: {
-    add_function() {
-      this.$emit("add_contact_details", this.input);
-      this.input = "";
+    add_item() {
+      if (
+        this.data_input === null ||
+        this.data_input === undefined ||
+        this.data_input === ""
+      ) {
+        console.log("Input Not Valid");
+        return;
+      }
+
+      for (let prop in this.data_items) {
+        var item = this.data_items[prop];
+        if (item.value === this.data_input) {
+          console.log("Item already exist");
+          return;
+        }
+      }
+
+      let key = `entry_${this.data_key}`;
+      this.data_items[key] = {
+        value: this.data_input,
+        primary: false,
+        key: key,
+      }; //--------------------------------
+      this.data_key++;
+
+      this.data_items = { ...this.data_items };
+
+      this.$emit("add_contact_details", this.data_items);
+      this.data_input = "";
     },
-    delete_entry() {},
+    delete_item($event, $target) {
+
+      delete this.data_items[$target.key];
+
+      this.data_items = { ...this.data_items };
+
+      this.$emit("add_contact_details", this.data_items);
+    },
   },
 };
 </script>
---
+++ src/components/TextDivider.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="text-divider py-6" >{{ text }}</div>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {};
+  },
+  props: ["text"],
+};
+</script>
+
+<style scoped>
+:root {
+  --text-divider-gap: 50px;
+}
+
+.text-divider {
+  display: flex;
+  align-items: center;
+}
+
+.text-divider::before,
+.text-divider::after {
+  content: "";
+  height: 1px;
+  background-color: silver;
+  flex-grow: 1;
+}
+
+.text-divider::before {
+  margin-right: 20px;
+}
+
+.text-divider::after {
+  margin-left: 20px;
+}
+</style>
